2c0f6895ccec2ff34981dcdc7556ccfa3b66f165
borgestj <borges.tj@gmail.com>
2019-01-25 22:30:45 -0200

    first commit

---
+++ ex.5/silvaFamily.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name='Roberto' lastName='Silva' />
+        <Member name='Leandro' lastName='Silva' />
+        <Member name='Dione' lastName='Silva' />
+        <Member name='Anselmo' lastName='Silva' />
+    </div>
+) 
+
